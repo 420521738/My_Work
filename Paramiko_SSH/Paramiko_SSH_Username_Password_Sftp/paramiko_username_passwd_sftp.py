2a941543df95ae079192bbb242a797cf24156dc4
420521738 <420521738@qq.com>
2018-04-08 17:39:31 +0800

    通过账号密码方式调用SFTP，远程传输文件，包括可以上传也可以下载

---
+++ Paramiko_SSH/Paramiko_SSH_Username_Password_Sftp/paramiko_username_passwd_sftp.py
@@ -0,0 +1,17 @@
+# cat paramiko_username_passwd_sftp.py
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+import paramiko
+import sys,os
+
+host = sys.argv[1]	###执行这个脚本后面加的IP地址
+user = 'ihavecar'	###先定义好用户名      
+password = 'x+y-z=71'	###先定义好密码
+port = 22612		###定义好端口
+
+t = paramiko.Transport((host,port))		###绑定传输实例t
+t.connect(username=user,password=password)	###建立一个传输连接
+
+sftp = paramiko.SFTPClient.from_transport(t)	###创建一个sftp实例，从t实例获取
+sftp.get('/home/ihavecar/1.txt','1.new.txt')	###将远程服务器的/home/ihavecar/1.txt下载到当前路径，并重命名文件为1.new.txt
+sftp.put('1.new.txt','/tmp/1.txt')		###将当前路径下的1.new.txt上传到远程服务器的/tmp/下，并重命名为1.txt
