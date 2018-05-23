b3d892c8ac0b05d21c4c66b88afaef69bd14fae2
420521738 <420521738@qq.com>
2018-05-23 17:05:48 +0800

    javascript和jQuery实例

---
+++ JavaScript与jQuery实例/extend.js
@@ -0,0 +1,26 @@
+﻿(function(){
+	jQuery.fn.extend({
+		allcheck:function(){
+			$(this).find(':checkbox').each(function(){
+				this.checked = true;
+			})
+		},
+		
+		uncheck:function(){
+			$(this).find(':checkbox').each(function(){
+				this.checked = false;
+			})
+		}	
+
+	});
+	
+	jQuery.extend({
+		jallcheck:function(arg){
+			$(arg).find(':checkbox').attr('checked','checked')
+		},
+		
+		unjallcheck:function(arg){
+			$(arg).find(':checkbox').attr('checked',false)
+		}
+	})
+})()
