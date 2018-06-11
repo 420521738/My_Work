0f4a20a10fc2ba15468f8d82fd0f1e2edb8a6851
420521738 <420521738@qq.com>
2018-06-11 18:04:06 +0800

    用户管理系统中，用户登录另一实现方式的AccountForm文件bug修改

--- 用户管理系统/UserManage/web/forms/AccountForm.py
+++ 用户管理系统/UserManage/web/forms/AccountForm.py
@@ -6,5 +6,6 @@ from django import forms
 # LoginForm是随便定义的，但是必须继承forms.Form
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=10)
+    # widget 是控件，意思是用什么展示
     password = forms.CharField(max_length=10,widget=forms.PasswordInput())
     # email = forms.EmailField(max_length=20)
